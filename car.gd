2aa0328ed1740a44c23f3bab9d306134b9105471
Espen Overaae <minthos@gmail.com>
2024-02-26 21:36:14 +0700

    I has a truck!

--- car.gd
+++ car.gd
@@ -1,5 +1,6 @@
 extends VehicleBody3D
 
 func _physics_process(delta):
-	steering = Input.get_axis("right", "left") * 0.4
+	steering = lerp(steering, Input.get_axis("right", "left") * 10.0 / (20 + linear_velocity.length()), 5 * delta)
 	engine_force = Input.get_axis("back", "forward") * 100
+
