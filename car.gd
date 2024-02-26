2b30a18e5b6b35595f5a0f008609e043ab1992e0
Espen Overaae <minthos@gmail.com>
2024-02-26 23:01:41 +0700

    this could be part of the resource harvesting gameplay loop

--- car.gd
+++ car.gd
@@ -2,5 +2,6 @@ extends VehicleBody3D
 
 func _physics_process(delta):
 	steering = lerp(steering, Input.get_axis("right", "left") * 10.0 / (20 + linear_velocity.length()), 5 * delta)
-	engine_force = Input.get_axis("back", "forward") * 100
+	engine_force = Input.get_axis("back", "forward") * 400
 
+# difficulty: It's hard to get all 4 balls inside the bed with engine force 200. It's easy with 400.
