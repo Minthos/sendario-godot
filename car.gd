3dba9b163b0e361a3aaa139431a3a0f0787235d3
Espen Overaae <minthos@gmail.com>
2024-02-27 02:28:42 +0700

    increased mass to realistic values

--- car.gd
+++ car.gd
@@ -2,6 +2,5 @@ extends VehicleBody3D
 
 func _physics_process(delta):
 	steering = lerp(steering, Input.get_axis("right", "left") * 10.0 / (20 + linear_velocity.length()), 5 * delta)
-	engine_force = Input.get_axis("back", "forward") * 400
+	engine_force = Input.get_axis("back", "forward") * 40000
 
-# difficulty: It's hard to get all 4 balls inside the bed with engine force 200. It's easy with 400.
